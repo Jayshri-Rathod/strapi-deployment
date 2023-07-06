7905fd9471468bd9773f71f181972804ce3ff837
JAYSHRI RATHOD <83463485+Jayshri-Rathod@users.noreply.github.com>
2023-07-06 12:38:24 +0530

    home-page-footer

---
+++ src/api/home-page-footer/routes/home-page-footer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-footer router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-page-footer.home-page-footer');
---
+++ src/api/home-page-footer/services/home-page-footer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-footer service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::home-page-footer.home-page-footer');
